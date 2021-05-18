5918dae7ac18a858b2997fe41258ce0cbee8f1a8
Vijay Rajagopalan <vijay.rajagopalan@aginic.com>
2021-05-18 07:08:19 +0000

    added macros

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -3,5 +3,5 @@
         order_id,
         payment_method,
         --`amount` is currently stored in cents, so we convert it to dollars
-        amount / 100 as amount
+        {{ cents_to_dollar('amount') }} as amount
     from {{ source('jaffle_shop', 'payments') }}
